a3d95582312b4ba7b72a149cefd60b3441e9963b
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2025-09-27 07:11:27 +0300

    upd `auth.py` status_code codes 401 to 403

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException, Depends 
+from fastapi import APIRouter, HTTPException, Depends, status 
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from sqlalchemy.exc import NoResultFound
 from sqlalchemy.orm import Session
@@ -18,10 +18,10 @@ async def login_email(db: db_dependency, credentials: OAuth2PasswordRequestForm 
     try:
         user = db.query(models.User).filter(models.User.email == credentials.username).one()
     except NoResultFound:
-        raise HTTPException(status_code=401, detail="Invalid credentials")
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Invalid credentials")
     
     if not hashing.verify_password(credentials.password, user.password):
-        raise HTTPException(status_code=401, detail="Invalid credentials")
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Invalid credentials")
     
     access_token = oauth2.create_access_token(data={"user_id": user.id})
     return {"access_token": access_token, "token_type": "bearer" } 

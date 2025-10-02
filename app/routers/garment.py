0c15a7c6e9c8293469d70275ebcce6c9efe5301d
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2025-10-02 07:16:43 +0300

    upd `garment.py` - users can only update their own garments

--- app/routers/garment.py
+++ app/routers/garment.py
@@ -38,6 +38,10 @@ async def read_garment(id: int, db: db_dependency):
 async def update_garment(id: int, garment_in: schemas.GarmentBase, db: db_dependency, current_user: models.User = Depends(oauth2.get_current_user)):
     try:
         garment = db.query(models.Garment).filter(models.Garment.id == id).one()
+
+        if garment.user_id != current_user.id:
+            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
+
         utils.update_object_attributes(garment_in, garment)
         db.commit()
         db.refresh(garment)

6d64e700c8031c5efcfef3f64a3d6f8bc29a95fb
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2025-10-02 21:16:23 +0300

    upd `garment.py` - users can get their own gaments

--- app/routers/garment.py
+++ app/routers/garment.py
@@ -22,14 +22,18 @@ async def create_garment(garment_in: schemas.GarmentCreate, db: db_dependency, c
     return garment
 
 @router.get("/", response_model=list[schemas.GarmentResponse])
-async def list_garments(db: db_dependency):
-    garment_list = db.query(models.Garment).all()
+async def list_garments(db: db_dependency, current_user: models.User = Depends(oauth2.get_current_user)):
+    garment_list = db.query(models.Garment).filter(models.Garment.user_id == current_user.id).all()
     return garment_list
 
 @router.get("/{id}", response_model=schemas.GarmentResponse)
-async def read_garment(id: int, db: db_dependency):
+async def read_garment(id: int, db: db_dependency, current_user: models.User = Depends(oauth2.get_current_user)):
     try:
         garment = db.query(models.Garment).filter(models.Garment.id == id).one()
+        
+        if garment.user_id != current_user.id:
+            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
+
         return garment
     except NoResultFound:
         raise HTTPException(status_code=404, detail="Garment not found")

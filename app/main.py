8e868266175332892674a82a25ef51c11d1f5c0a
maxstepanyuk linux <40894200+maxstepanyuk@users.noreply.github.com>
2025-11-27 23:43:11 +0200

    `app.py` use `initialize_outfit_templates`

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ from .config import config
 from .database import engine, get_db
 from . import models
 from .routers import user, garment, auth, outfit, image, gender, category, garment_type, color, season, usage
-from .seed_data import initialize_table, initialize_ml_mapping_table
+from .seed_data import initialize_table, initialize_ml_mapping_table, initialize_outfit_templates
 
 SEED_DATA = config["SEED_DATA"]
 
@@ -44,6 +44,7 @@ if SEED_DATA == "yes":
     event.listen(models.Season.__table__, 'after_create', initialize_table)
     event.listen(models.Usage.__table__, 'after_create', initialize_table)
     event.listen(models.MLMapping.__table__, 'after_create', initialize_ml_mapping_table)
+    event.listen(models.OutfitTemplateCategoriesSub.__table__, 'after_create', initialize_outfit_templates)
 
 
 models.Base.metadata.create_all(bind=engine) # Create the tables
